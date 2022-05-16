15dd32449d78ba3806d8c2dc3c88d814c7c3bfb6
Ian Jones <ian@egghead.io>
2022-05-15 20:07:35 -0400

    lessons 08-09

---
+++ 08-vercel/app/components/PostForm/PostForm.tsx
@@ -0,0 +1,45 @@
+import type {Props} from './types'
+
+function PostForm({error, fields, method = 'post', ...props}: Props) {
+  return (
+    <form className="flex flex-col gap-4" method={method} {...props}>
+      <div className="flex flex-col">
+        <label htmlFor="title" className="mb-2 text-gray-600">
+          Title
+        </label>
+        <input
+          defaultValue={fields?.title}
+          className="p-4"
+          name="title"
+          placeholder="Title of your post"
+        />
+        {false && error?.fieldErrors?.title && (
+          <p className="text-red-500">{error.fieldErrors.title}</p>
+        )}
+      </div>
+      <div className="mb-4 flex flex-col">
+        <label htmlFor="body" className="mb-2 text-gray-600">
+          Body
+        </label>
+        <textarea
+          defaultValue={fields?.body}
+          className="p-4"
+          name="body"
+          placeholder="Write something amazing"
+        />
+        {false && error?.fieldErrors?.body && (
+          <p className="text-red-500">{error.fieldErrors.body}</p>
+        )}
+      </div>
+      {error?.formError && <p className="text-red-500">{error.formError}</p>}
+      <button
+        type="submit"
+        className="transition rounded text-blue-700 font-bold py-4 px-6 transparent hover:bg-gray-100"
+      >
+        Create Post
+      </button>
+    </form>
+  )
+}
+
+export default PostForm
---
+++ 08-vercel/app/routes/index.tsx
@@ -0,0 +1,77 @@
+import type {ActionFunction, LoaderFunction} from '@remix-run/node'
+import {redirect, json} from '@remix-run/node'
+import {useActionData, useLoaderData} from '@remix-run/react'
+import {createPost, getPosts} from '~/services/posts.server'
+import type {Post} from '~/services/posts.server'
+import {Post as PostComponent} from '~/components/Post'
+import {PostForm} from '~/components/PostForm'
+import {CreatePost} from '~/services/validations'
+
+type LoaderData = {
+  posts: Post[]
+}
+
+type ActionData = {
+  error: {
+    formError?: string[]
+    fieldErrors?: {
+      title?: string[]
+      body?: string[]
+    }
+  }
+  fields: {
+    title?: string
+    body?: string
+  }
+}
+
+export const action: ActionFunction = async ({request}) => {
+  const form = await request.formData()
+  const rawTitle = form.get('title')
+  const rawBody = form.get('body')
+  const result = CreatePost.safeParse({title: rawTitle, body: rawBody})
+
+  if (!result.success) {
+    return json(
+      {
+        error: result.error.flatten(),
+        fields: {
+          title: rawTitle,
+          body: rawBody,
+        },
+      },
+      {status: 400},
+    )
+  }
+
+  await createPost({title: result.data.title ?? null, body: result.data.body})
+
+  return redirect('/')
+}
+
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = {posts: await getPosts()}
+  return json(data)
+}
+
+export default function Index() {
+  const {posts} = useLoaderData<LoaderData>()
+  const formData = useActionData<ActionData>()
+  return (
+    <div className="p-8 flex flex-col items-center gap-8">
+      <h1 className="text-xl">Remix Social</h1>
+      <PostForm
+        action="/?index"
+        error={formData?.error}
+        fields={formData?.fields}
+      />
+      <ul className="flex flex-col gap-4">
+        {posts.map((post) => (
+          <li key={post.body}>
+            <PostComponent header={post?.title}>{post.body}</PostComponent>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
